47c01e6e6fcde544ab690f9b62d52b1db0637c22
Melina-C13 <cabalcetamelina@gmail.com>
2024-11-11 23:32:22 -0600

    Entities test

--- src/landing-services/entities/landing-service.entity.ts
+++ src/landing-services/entities/landing-service.entity.ts
@@ -1,1 +1,17 @@
-export class LandingService {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('LandingService')
+export class LandingService {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    icons: string;
+
+    @Column()
+    tittle: string;
+
+    @Column()
+    description: string;
+}
