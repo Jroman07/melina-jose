8eb9a4edc2c2ccc6d66e770c433c039471f442c4
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2024-11-11 23:44:33 -0600

    landing-services module

--- src/landing-services/landing-services.module.ts
+++ src/landing-services/landing-services.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LandingServicesService } from './landing-services.service';
 import { LandingServicesController } from './landing-services.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LandingService } from './entities/landing-service.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LandingService])],
   controllers: [LandingServicesController],
   providers: [LandingServicesService],
 })
