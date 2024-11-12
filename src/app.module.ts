0d13ea1e53fb626e5ae3f0d96523ba6fcf963ef6
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2024-11-11 23:01:16 -0600

    add-typeOrmDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LandingServicesModule } from './landing-services/landing-services.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [LandingServicesModule],
+  imports: [
+    LandingServicesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'JosDani_1007',
+      database: 'binarios',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
